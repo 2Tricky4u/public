a415c814c4c308310bb0ba1feeed8a6c5d5cf14b
Xavier <xavier.ogay@epfl.ch>
2022-10-13 17:34:52 +0200

    Q5 of Week 3

--- lectures/Testing/exercises/Q5/src/main/java/Vector3.java
+++ lectures/Testing/exercises/Q5/src/main/java/Vector3.java
@@ -3,5 +3,58 @@
  * Write the specification here
  */
 public final class Vector3 {
+    private final double x, y, z, length;
 
+    public Vector3(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.length = Math.sqrt(x*x + y*y + z*z);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public boolean isZero() {
+        return length == 0;
+    }
+
+    public double dot(Vector3 v) {
+        return (v.x*x + v.y*y + v.z*z);
+    }
+
+    public Vector3 cross(Vector3 v) {
+        double nx = y*v.z - z*v.y;
+        double ny= -(x*v.z - z*v.x);
+        double nz= x*v.y - y*v.x;
+        return new Vector3(nx,ny,nz);
+    }
+
+    public Vector3 normalize(){
+        return new Vector3(x/length,y/length,z/length);
+    }
+
+    public Vector3 scale(double n){
+        return new Vector3(n*x, n*y, n*z);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!obj.getClass().equals(Vector3.class)) return false;
+        Vector3 v = (Vector3) obj;
+        return x == v.getX() && y == v.getY() && z == v.getZ();
+    }
 }
--- lectures/Testing/exercises/Q5/src/test/java/Vector3Test.java
+++ lectures/Testing/exercises/Q5/src/test/java/Vector3Test.java
@@ -9,4 +9,43 @@ final class Vector3Test {
         assertThat(1, is(1));
     }
 
+    @Test
+    public void isZeroVectorDetected() {
+        Vector3 v = new Vector3(0,0,0);
+        assertThat(v.isZero(),is(true));
+    }
+
+    @Test
+    public void isDotPoductWithSimpleVectWorking(){
+        Vector3 v1 = new Vector3(1,2,3), v2 = new Vector3(3,2,1);
+        assertThat(v1.dot(v2), is((double) 3+4+3));
+    }
+
+    @Test
+    public void isTheLengthOfSimpleVectWorking(){
+        Vector3 v = new Vector3(2,3,4);
+        assertThat(v.getLength(), is(Math.sqrt(2*2+3*3+4*4)));
+    }
+
+    @Test
+    public void isCrossProdWorkingWithSimpleVect(){
+        Vector3 v1 = new Vector3(3,2,1), v2 = new Vector3(1,2,3);
+        assertThat(v1.cross(v2), is(new Vector3(4,-8,4)));
+    }
+
+    @Test
+    public void isSimpleNormalizationWorking(){
+        Vector3 v = new Vector3(2,1,3);
+        Vector3 normalized = v.normalize();
+        assertThat(normalized.getX(), closeTo(0.5345224838248488, 0.005));
+        assertThat(normalized.getY(), closeTo(0.2672612419124244, 0.005));
+        assertThat(normalized.getZ(), closeTo(0.8017837257372732, 0.005));
+    }
+
+    @Test
+    public void scaleByNumberWorks(){
+        Vector3 v = new Vector3(1,2,3);
+        assertThat(v.scale(3), is(new Vector3(3,6,9)));
+
+    }
 }
